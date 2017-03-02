e5da923b0d106b47d37f07cf06ec65cc2ff8275f
chenjunhang <1728533217en@gmail.com>
2017-03-02 20:42:38 +0800

    get api host from template globals & use template string

--- main.py
+++ main.py
@@ -3,15 +3,24 @@
 
 from flask import Flask
 from flask.templating import render_template
-
+from settings import API_HOST
 
 app = Flask(__name__, template_folder="templates")
+
+
+@app.template_global(name="api_host")
+def api_host():
+    return API_HOST
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/keywords")
+def keywords():
+    return render_template("keywords.html")
 
 if __name__ == "__main__":
     app.run(port=7000, debug=True)
