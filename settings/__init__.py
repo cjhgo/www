8136c0649ce11ff5520932b6c5d1812b5cbe3019
chenjunhang <1728533217en@gmail.com>
2017-03-02 20:48:54 +0800

    add settings

---
+++ settings/__init__.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+#created at 17-3-2 14:17
+
+import os
+
+API_HOST = "info.local.cjhang.com"
+
+www_env = os.getenv("WWW_ENV", "local")
+if www_env == "production":
+    from production import *
---
+++ settings/production.py
@@ -0,0 +1,5 @@
+#coding: utf-8
+#created at 17-3-2 14:19
+
+
+API_HOST = "api.cjhang.com"
