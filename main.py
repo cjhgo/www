7c267c90c9bac8a8c52c63aed4f6b5cf3e1db40e
chenjunhang <1728533217en@gmail.com>
2017-03-02 11:21:28 +0800

    use flask

---
+++ main.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+#created at 17-3-2 11:12
+
+from flask import Flask
+from flask.templating import render_template
+
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=6000, debug=True)
